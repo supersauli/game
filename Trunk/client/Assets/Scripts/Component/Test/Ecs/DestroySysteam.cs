902e5af6e4779321f7ac1de868def75087748a95
supersauli <sauli.good@163.com>
2018-09-21 02:11:58 +0800

    [add] ecs

--- Trunk/client/Assets/Scripts/Component/Test/Ecs/DestroySysteam.cs
+++ Trunk/client/Assets/Scripts/Component/Test/Ecs/DestroySysteam.cs
@@ -3,16 +3,19 @@ using Unity.Collections;
 using Unity.Burst;
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
-[UpdateBefore(typeof(MoveForwardSysteam))]
-//public class DestroySysteam : JobComponentSystem
-public class DestroySysteam : ComponentSystem
+//[UpdateBefore(typeof(MoveForwardSysteam))]
+public class DestroySysteam :JobComponentSystem
+//public class DestroySysteam : ComponentSystem
 {
+
     struct DestroyGroup
     {
-        [ReadOnly] public ComponentDataArray<Position> _postion;
+         public ComponentDataArray<Position> _postion;
         [ReadOnly] public ComponentDataArray<SelfEntity> _selfEntity;
+     public GameObjectArray gameObjects;
         public readonly int Length;
     }
     [Inject] DestroyGroup _destroyGroup;
@@ -23,52 +26,57 @@ public class DestroySysteam : ComponentSystem
     //}
     //[Inject] OtherGroup _otherGroup
 
-    protected override void OnUpdate()
-    {
-        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
-        for (int i = 0; i < _destroyGroup.Length; i++)
-        {
-            if (_destroyGroup._postion[i].Value.x > 300)
-            {
-                entityManager.DestroyEntity(_destroyGroup._selfEntity[i]._entity);
-            }
-        }
-
-
-    }
-
-
-    //protected override JobHandle OnUpdate(JobHandle inputDeps)
+    //protected override void OnUpdate()
     //{
-    //    int boidCount = _destroyGropu.Length;
-    //    var position = new NativeArray<Position>(boidCount, Unity.Collections.Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
-    //    var selfEntity = new NativeArray<SelfEntity>(boidCount, Unity.Collections.Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
-
-    //    var job = new Destroy
+    //    var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+    //    for (int i = 0; i < _destroyGroup.Length; i++)
     //    {
-    //       _postion = position,
-    //        _selfEntity = selfEntity
-    //    };
-    //    return job.Schedule(boidCount, 64, inputDeps);
+    //        if (_destroyGroup._postion[i].Value.x > 300)
+    //        {
+    //            entityManager.DestroyEntity(_destroyGroup._selfEntity[i]._entity);
+    //        }
+    //    }
+
+
     //}
 
 
-    //[BurstCompile]
-    //struct Destroy : IJobParallelFor
-    //{
-    //   [ReadOnly] public NativeArray<Position> _postion;
-    //   [ReadOnly] public NativeArray<SelfEntity> _selfEntity;
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        int boidCount = _destroyGroup.Length;
+        //var position = new NativeArray<Position>(boidCount, Unity.Collections.Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+        //var selfEntity = new NativeArray<SelfEntity>(boidCount, Unity.Collections.Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
 
-    //    public void Execute(int index)
-    //    {
-    //        var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+        var job = new Destroy
+        {
+            //_position = position,
+            //_selfEntity = selfEntity
+        };
+        return job.Schedule(boidCount, 64, inputDeps);
+    }
 
-    //            var postion = _postion[index];
-    //            if (postion.Value.x > 120)
-    //            {
-    //                entityManager.DestroyEntity(_selfEntity[index]._entity);
-    //            }
-    //        }
-    //    }
+
+    [BurstCompile]
+    struct Destroy : IJobParallelFor
+    {
+        public NativeArray<Position> _position;
+        [ReadOnly] public NativeArray<SelfEntity> _selfEntity;
+        private GameObjectArray gameObjects;
+        public void Execute(int index)
+        {
+            //var entityManager = World.Active.GetOrCreateManager<EntityManager>();
+            //var postion = _position[index];
+            //if (postion.Value.x > 120)
+            //{
+            //    entityManager.DestroyEntity(_selfEntity[index]._entity);
+            ////}
+            //_position[index] = new Position
+            //{
+            //    Value = new float3(postion.Value.x + 1, postion.Value.y, postion.Value.z)
+            //};
+        }
+    }
+
+   
 
 };
--- Trunk/client/Assets/Scripts/Component/Test/Ecs/MoveForwardSysteam.cs
+++ Trunk/client/Assets/Scripts/Component/Test/Ecs/MoveForwardSysteam.cs
@@ -1,32 +1,53 @@
-﻿using Unity.Collections;
+﻿using Unity.Burst;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
-using UnityEngine;
-using UnityEditor;
+using Unity.Collections;
 
-public class MoveForwardSysteam : ComponentSystem
+public class MoveForwardSysteam : JobComponentSystem
 {
-    struct MoveGroup
+    struct PositionGroup
     {
         public ComponentDataArray<Position> _position;
         public readonly int Length;
     }
 
-    [Inject] MoveGroup enemies;
-    protected override void OnUpdate()
+    [Inject] PositionGroup _positionGroup;
+      protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        for (int i = 0; i < enemies.Length; i++)
-        {
-            var oldPosition = enemies._position[i];
-            if (oldPosition.Value.x < 400)
-            {
-                enemies._position[i] = new Position
-                {
-                    Value = new float3(oldPosition.Value.x + 1, oldPosition.Value.y, oldPosition.Value.z)
-                };
-            }
-        }
-         
+        //for (int i = 0; i < enemies.Length; i++)
+        //{
+        //    var oldPosition = enemies._position[i];
+        //    //if (oldPosition.Value.x < 400)
+        //    {
+        //        enemies._position[i] = new Position
+        //        {
+        //            Value = new float3(oldPosition.Value.x + 1, oldPosition.Value.y, oldPosition.Value.z)
+        //        };
+        //    }
+        //}
+        var movePositon = new MovePosition {_position = _positionGroup._position};
+       return movePositon.Schedule(_positionGroup.Length,64,inputDeps);
     }
+
+    [BurstCompile]
+    struct MovePosition:IJobParallelFor
+    {
+        public ComponentDataArray<Position> _position;
+        public void Execute(int index)
+        {
+            var pos = _position[index];
+            _position[index] = new Position
+            {
+                Value = new float3(pos.Value.x + 1,0,pos.Value.z)
+            };
+        }
+    }
+
+
+
+
+
+
 }
